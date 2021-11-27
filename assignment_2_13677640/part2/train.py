49c4f7fe0541de586cf1a7cda0af749a5fafbe06
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2021-11-27 02:00:21 +0100

    Adds train script

--- assignment_2_13677640/part2/train.py
+++ assignment_2_13677640/part2/train.py
@@ -14,6 +14,7 @@
 # Date Adapted: 2021-11-11
 ###############################################################################
 
+import os
 from datetime import datetime
 import argparse
 from tqdm.auto import tqdm
@@ -61,20 +62,112 @@ def train(args):
     #######################
     # PUT YOUR CODE HERE  #
     #######################
+    # set seed
     set_seed(args.seed)
+    
+    # set directory to save model checkpoints
+    file_name = os.path.basename(args.txt_file)
+    ckpt_dir = os.path.join(os.path.dirname(__file__), "checkpoints", file_name)
+    os.makedirs(ckpt_dir, exist_ok=True)
+    ckpt_path_template = os.path.join(
+        ckpt_dir, "ckpt_epoch_%s.pt.tar"
+    )
+
     # Load dataset
     # The data loader returns pairs of tensors (input, targets) where inputs are the
     # input characters, and targets the labels, i.e. the text shifted by one.
     dataset = TextDataset(args.txt_file, args.input_seq_length)
-    data_loader = DataLoader(dataset, args.batch_size, 
-                             shuffle=True, drop_last=True, pin_memory=True,
-                             collate_fn=text_collate_fn)
+    data_loader = DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        drop_last=True,
+        pin_memory=True,
+        collate_fn=text_collate_fn,
+    )
+    args.vocabulary_size = len(dataset._char_to_ix)
+
     # Create model
-    model = ...
+    model = TextGenerationModel(args)
+    model = model.to(args.device)
+
     # Create optimizer
-    optimizer = ...
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    
+    # Define loss criterion
+    criterion = nn.CrossEntropyLoss(reduction="none")
+    
+    # To collect epoch losses
+    epoch_losses = []
+
+    # To collect best model (based on training loss)
+    best_ckpt = {
+        "epoch": None,
+        "loss": np.inf,
+    }
+    
     # Training loop
-    pass
+    for e in range(1, args.num_epochs + 1):
+        # Train model
+        model.train()
+        
+        # To collect batch losses
+        batch_losses = []
+        epoch_iterator = tqdm(data_loader, desc=f"Epoch {e}")
+
+        for batch in epoch_iterator:
+            x, y = batch
+            x = x.to(args.device)
+            y = y.to(args.device)
+            
+            # zero out parameter gradients
+            optimizer.zero_grad()
+            
+            # model forward pass
+            y_hat = model(x)
+            
+            # compute loss
+            loss = criterion(y_hat.permute((0, 2, 1)), y)
+            # sum loss across time steps and mean across examples in batch
+            loss = loss.mean(dim=0).mean(dim=0)
+            
+            # backpropagate and update parameters
+            loss.backward()
+            optimizer.step()
+
+            loss_scalar = loss.item()
+            batch_losses.append(loss_scalar)
+            
+            # update the display
+            display = f"::::: [Training] | Epoch {e} | Loss: {loss_scalar:.3f} | "
+            epoch_iterator.set_description(display)
+
+        epoch_losses.append(np.mean(batch_losses))
+        
+        # update best model and save
+        if best_ckpt["loss"] > epoch_losses[-1]:
+            best_ckpt["epoch"] = e
+            best_ckpt["loss"] = epoch_losses[-1]
+            best_ckpt.update({
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict(),
+            })
+            ckpt_path = ckpt_path_template % f"best"
+            torch.save(best_ckpt, ckpt_path)
+            print(f"::::: Loss improved to {epoch_losses[-1]:.4f}")
+            print(f"::::: Saved checkpoint to {ckpt_path}")
+
+        # Save model checkpoint
+        if e % args.save_every == 0 or e in [1, args.num_epochs]:
+            ckpt_path = ckpt_path_template % e
+            ckpt = {
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict(),
+                "epoch": e,
+                "loss": epoch_losses[-1],
+            }
+            torch.save(ckpt, ckpt_path)
+            print(f"::::: Saved checkpoint to {ckpt_path}")
     #######################
     # END OF YOUR CODE    #
     #######################
@@ -97,6 +190,7 @@ if __name__ == "__main__":
 
     # Additional arguments. Feel free to add more arguments
     parser.add_argument('--seed', type=int, default=0, help='Seed for pseudo-random number generator')
+    parser.add_argument('--save_every', type=int, default=5, help='To save ckpt after every E epochs')
 
     args = parser.parse_args()
     args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # Use GPU if available, else use CPU
